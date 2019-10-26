fe39386935a052ce8d883d746c4b9ed276e0d1db
bai1515610177 <1515610177@qq.com>
2019-10-26 11:19:41 +0800

    登录模块样式完成

---
+++ src/views/login/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- 全局容器 -->
+  <div class="container">
+    <el-card>
+      <img src="../../assets/logo_index.png" alt />
+      <el-form ref="form" :model="LoginForm">
+        <el-form-item>
+          <el-input v-model="LoginForm.moblie" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="LoginForm.code"
+            style="width:67%;margin-right:5px"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%;">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      LoginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #ccc;
+  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  .el-card {
+    width: 400px;
+    height: 320px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto 15px;
+    }
+  }
+}
+</style>
